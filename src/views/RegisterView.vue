e0e62ea5b287a29e225c7d1d8e8537fb9be661ef
Chuck Smith <csmit223@nd.edu>
2024-06-11 16:53:38 -0400

    Add basic flowbite

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -4,7 +4,7 @@
     <form @submit.prevent="handleSubmit">
       <input type="email" placeholder="Email address" v-model.trim="email">
       <input type="password" placeholder="Password" v-model.trim="password">
-      <button type="submit" :disabled="authStore.loadingUser">Create Account</button>
+      <button type="submit" :disabled="authStore.loadingUser" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Create Account</button>
     </form>
   </main>
 </template>
