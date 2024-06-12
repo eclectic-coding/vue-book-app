bdf9230f78edb79af73912f21c00c515ed77b4b9
Chuck Smith <csmit223@nd.edu>
2024-06-11 21:21:18 -0400

    Finish up base styling

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -16,7 +16,7 @@
       <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
         <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
           <li>
-            <RouterLink to="/" aria-current="page">Home</RouterLink>
+            <RouterLink to="/" class="nav-link" aria-current="page">Home</RouterLink>
           </li>
           <li v-if="!authStore.userData">
             <RouterLink to="/register" class="nav-link">Register</RouterLink>
