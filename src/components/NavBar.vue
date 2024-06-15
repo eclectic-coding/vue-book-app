3f411e04a32c48c230957de1256a6901b972964d
Chuck Smith <csmit223@nd.edu>
2024-06-15 08:17:46 -0400

    Update router links to use name

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -42,16 +42,18 @@
           class="mt-4 flex flex-col rounded-lg border border-gray-100 bg-gray-50 p-4 font-medium dark:border-gray-700 dark:bg-gray-800 md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:p-0 md:dark:bg-gray-900 rtl:space-x-reverse"
         >
           <li>
-            <RouterLink to="/" class="nav-link" aria-current="page">Home</RouterLink>
+            <RouterLink :to="{ name: 'home' }" class="nav-link" aria-current="page">
+              Home
+            </RouterLink>
           </li>
           <li v-if="!authStore.userData">
-            <RouterLink to="/register" class="nav-link">Register</RouterLink>
+            <RouterLink :to="{ name: 'register' }" class="nav-link">Register</RouterLink>
           </li>
           <li v-if="!authStore.userData">
-            <RouterLink to="/signin" class="nav-link">Sign In</RouterLink>
+            <RouterLink :to="{ name: 'signin' }" class="nav-link">Sign In</RouterLink>
           </li>
           <li v-if="authStore.userData">
-            <RouterLink to="/dashboard" class="nav-link">Dashboard</RouterLink>
+            <RouterLink :to="{ name: 'dashboard' }" class="nav-link">Dashboard</RouterLink>
           </li>
           <li v-if="authStore.userData">
             <a href="#" @click="logout" class="nav-link">Log out</a>
@@ -63,8 +65,8 @@
 </template>
 
 <script setup>
-import {RouterLink} from 'vue-router'
-import {useAuthStore} from '@/stores/authStore.js'
+import { RouterLink } from 'vue-router'
+import { useAuthStore } from '@/stores/authStore.js'
 
 const authStore = useAuthStore()
 
